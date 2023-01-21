851593c4a55e0831b25cb16e3906d035b767e4f3
ibrahiminthegame <kibrahimilhan@gmail.com>
2023-01-21 22:48:31 +0300

    calculator21.01.2023

---
+++ clock.py
@@ -0,0 +1,6 @@
+import time
+
+while True:
+    current_time = time.strftime("%H:%M:%S")
+    print(current_time)
+    time.sleep(1)
---
+++ pythoncalcu/calculator.py
@@ -0,0 +1,39 @@
+def calculator():
+    while True:
+        print("HELLO world")
+        print("Options:")
+        print("Enter 'add' to add two numbers")
+        print("Enter 'subtract' to subtract two numbers")
+        print("Enter 'multiply' to multiply two numbers")
+        print("Enter 'divide' to divide two numbers")
+        print("Enter 'quit' to end the program")
+        user_input = input(": ")
+        if user_input == "quit":
+            break
+        elif user_input == "add":
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+            result = num1 + num2
+            print("The answer is " + str(result))
+        elif user_input == "subtract":
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+            result = num1 - num2
+            print("The answer is " + str(result))
+        elif user_input == "multiply":
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+            result = num1 * num2
+            print("The answer is " + str(result))
+        elif user_input == "divide":
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+            if num2 == 0:
+                print("Cannot divide by zero!")
+            else:
+                result = num1 / num2
+                print("The answer is " + str(result))
+        else:
+            print("Unknown input")
+
+calculator()
